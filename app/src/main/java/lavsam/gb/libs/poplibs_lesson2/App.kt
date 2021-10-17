10d880c7e808f21092208723053ed02f2b3742bc
lavsam-dev <lavsam@gmail.com>
2021-10-17 22:03:14 +0400

    App + Repositories

---
+++ app/src/main/java/lavsam/gb/libs/poplibs_lesson2/App.kt
@@ -0,0 +1,24 @@
+package lavsam.gb.libs.poplibs_lesson2
+
+import android.app.Application
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
+
+class App : Application() {
+    companion object {
+        lateinit var instance: App
+    }
+
+    val cicerone: Cicerone<Router> by lazy {
+        Cicerone.create()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
+
+
+    fun getNavigatorHolder() = cicerone.navigatorHolder
+    fun getRouter() = cicerone.router
+}
--- app/src/main/java/lavsam/gb/libs/poplibs_lesson2/Screens.kt
+++ app/src/main/java/lavsam/gb/libs/poplibs_lesson2/Screens.kt
@@ -5,6 +5,7 @@ import ru.terrakok.cicerone.android.support.SupportAppScreen
 class Screens {
 
     class RepositoriesScreen() : SupportAppScreen() {
+        override fun getFragment() = RepositoriesFragment.newInstance()
     }
 
     class RepositoryScreen(val repository: GithubRepository) : SupportAppScreen() {
